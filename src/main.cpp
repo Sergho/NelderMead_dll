10db1780badfa472fee6fed4d3a323a3f97c9b16
Sergho <sergche04@gmail.com>
2025-05-05 23:29:09 +0300

    fix: throw incorrect expression error

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main() {
-  ExpressionTree* tree = ExpressionTree::create_tree("5*6");
+  ExpressionTree* tree = ExpressionTree::create_tree("5*6+");
   cout << tree->json_tree() << endl;
   return 0;
 }
